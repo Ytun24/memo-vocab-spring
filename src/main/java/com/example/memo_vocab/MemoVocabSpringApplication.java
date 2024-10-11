f932d1ec891faac78a636e567dbc1a942e6be006
Ytun24 <naying24@gmail.com>
2024-10-12 01:22:43 +0700

    Init spring project with postgres sql

---
+++ src/main/java/com/example/memo_vocab/MemoVocabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.memo_vocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoVocabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoVocabSpringApplication.class, args);
+	}
+
+}
