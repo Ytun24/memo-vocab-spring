8ec80c4bd98319d47a26c324be3585237115cfa1
Ytun24 <tunyaporn.subsakorn@gmail.com>
2025-05-05 15:08:18 +0700

    Add spring security setting

--- src/main/java/com/example/memovocab/mapper/UserMapper.java
+++ src/main/java/com/example/memovocab/mapper/UserMapper.java
@@ -10,7 +10,6 @@ import org.mapstruct.factory.Mappers;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-
     User mapToUser(UserPostRequestDto userPostRequestDto);
 
     UserPostResponseDto mapToUserPostResponseDto(User user);
