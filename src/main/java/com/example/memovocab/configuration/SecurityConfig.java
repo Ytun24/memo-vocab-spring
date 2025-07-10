3244adc1354ce255c70bad141c4c0bb072796081
ginying <tunyaporn.subsakorn@gmail.com>
2025-07-10 23:53:41 +0700

    Add OAuth resource dependency and Apply configuration

--- src/main/java/com/example/memovocab/configuration/SecurityConfig.java
+++ src/main/java/com/example/memovocab/configuration/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.example.memovocab.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +12,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                        .anyRequest().authenticated());
+
+        http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer ->
+                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 
         return http.build();
     }
