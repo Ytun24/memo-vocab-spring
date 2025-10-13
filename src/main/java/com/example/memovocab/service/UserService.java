b4cc8289ade439412c42eaf8ddb178b51e8c0980
ginying <tunyaporn.subsakorn@gmail.com>
2025-10-13 21:43:32 +0700

    Update user cache name

--- src/main/java/com/example/memovocab/service/UserService.java
+++ src/main/java/com/example/memovocab/service/UserService.java
@@ -24,8 +24,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
 
-    public static final String USER_CACHE = "users";
-
+    public static final String USER_CACHE = "userCache";
+    
     private final UserRepository userRepository;
 
     public List<UserDto> searchUser(UserSearchDto searchCriteria) {
@@ -36,7 +36,7 @@ public class UserService {
         return UserMapper.INSTANCE.mapToUserDtoList(userRepository.findAll(pageable).getContent());
     }
 
-    @Cacheable(value = USER_CACHE, key = "#id")
+    @Cacheable(cacheNames = "userCache", key = "#id")
     public UserDto getUser(int id) {
         User user = userRepository.findById(id).get();
         return UserMapper.INSTANCE.mapToUserDto(user);
